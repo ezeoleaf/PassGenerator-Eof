d4a13025f8b78345499d1886c265eb0744dd59ad
Eze BIT <efigueroa@bit.com.ar>
2015-12-28 12:06:12 -0300

    Add dictionary data

--- js/passGenerator.js
+++ js/passGenerator.js
@@ -18,103 +18,6 @@ function newDigits()
 	return newDigit;
 }
 
-function setDictionary()
-{
-
-	numberDictionary = {
-		"00110000": "0",
-		"00110001": "1",
-		"00110010": "2",
-		"00110011": "3",
-		"00110100": "4",
-		"00110101": "5",
-		"00110110": "6",
-		"00110111": "7",
-		"00111000": "8",
-		"00111001": "9",
-	}
-
-	symbolDictionary = {
-		"00100001": "!",
-		"00100011": "#",
-		"00100100": "$",
-		"00100101": "%",
-		"00100110": "&",
-		"00101010": "*",
-		"00101011": "+",
-		"00101101": "-",
-		"00101110": ".",
-		"00101111": "/",
-		"00111010": ":",
-		"00111011": ";",
-		"00111101": "=",
-		"00111111": "?",
-		"01000000": "@",
-		"01011011": "[",
-		"01011101": "]",
-		"01011111": "_",
-		"01111110": "~",
-	}
-	
-	letterUDictionary = {
-		"01000001": "A",
-		"01000010": "B",
-		"01000011": "C",
-		"01000100": "D",
-		"01000101": "E",
-		"01000110": "F",
-		"01000111": "G",
-		"01001000": "H",
-		"01001001": "I",
-		"01001010": "J",
-		"01001011": "K",
-		"01001100": "L",
-		"01001101": "M",
-		"01001110": "N",
-		"01001111": "O",
-		"01010000": "P",
-		"01010001": "Q",
-		"01010010": "R",
-		"01010011": "S",
-		"01010100": "T",
-		"01010101": "U",
-		"01010110": "V",
-		"01010111": "W",
-		"01011000": "X",
-		"01011001": "Y",
-		"01011010": "Z",
-	}
-	
-	letterLDictionary =	{
-		"01100001": "a",
-		"01100010": "b",
-		"01100011": "c",
-		"01100100": "d",
-		"01100101": "e",
-		"01100110": "f",
-		"01100111": "g",
-		"01101000": "h",
-		"01101001": "i",
-		"01101010": "j",
-		"01101011": "k",
-		"01101100": "l",
-		"01101101": "m",
-		"01101110": "n",
-		"01101111": "o",
-		"01110000": "p",
-		"01110001": "q",
-		"01110010": "r",
-		"01110011": "s",
-		"01110100": "t",
-		"01110101": "u",
-		"01110110": "v",
-		"01110111": "w",
-		"01111000": "x",
-		"01111001": "y",
-		"01111010": "z",
-	}
-}
-
 function getValue(binaryCode)
 {
 	digitToReturn = "";
@@ -198,6 +101,10 @@ function getPass(longPass)
 function loadDictionaries()
 {
 	dictionary = {};
+	cantOfSymbol = 0;
+	cantOfNumbers = 0;
+	cantOfUpper = 0;
+	cantOfLower = 0;
 
 	if(check == 1)
 	{
@@ -207,16 +114,11 @@ function loadDictionaries()
 				dictionary[key] = value;
 			}
 		});
-		cantOfSymbol = 0;
 	}
 	else
 	{
 		cantOfSymbol = 1;
 	}
-
-	cantOfNumbers = 0;
-	cantOfUpper = 0;
-	cantOfLower = 0;
 
 	$.each( numberDictionary, function( key, value ) {
 		if($.inArray(key, excludedSymbols) == -1)
@@ -254,21 +156,21 @@ function loadSimbols()
 function getNumberValue(val)
 {
 	if($.inObject(val, symbolDictionary) != null)
-		{
-			return 1; //Symbol
-		}
-		if($.inObject(val, numberDictionary) != null)
-		{
-			return 2; //Number
-		}
-		if($.inObject(val, letterUDictionary) != null)
-		{
-			return 3; //Character
-		}
-		if($.inObject(val, letterLDictionary) != null)
-		{
-			return 3; //Character
-		}
+	{
+		return 1; //Symbol
+	}
+	if($.inObject(val, numberDictionary) != null)
+	{
+		return 2; //Number
+	}
+	if($.inObject(val, letterUDictionary) != null)
+	{
+		return 3; //Character
+	}
+	if($.inObject(val, letterLDictionary) != null)
+	{
+		return 3; //Character
+	}
 }
 
 function popSimbol(nameSimbol,simbolKey, simbolValue)
